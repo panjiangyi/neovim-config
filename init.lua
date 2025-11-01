6ec26902b068797fd556cf81fa1f93106e6eb6ca
James Pan <panjiangyi@gmail.com>
2025-11-01 19:59:22 +0800

    Add nvim-treesitter plugin configuration and remove absolute line numbers from init.lua for cleaner display settings

--- init.lua
+++ init.lua
@@ -4,7 +4,6 @@ require('keybindings')
 require('plugin-config.telescope')
 
 -- Basic settings from init.vim
-vim.opt.number = true          -- 显示绝对行号
 vim.opt.relativenumber = true  -- 显示相对行号（可选）
 vim.opt.clipboard = "unnamedplus"
 
