8ee29b605cc2dbf1f3a54c337b4b522ca98bf599
James Pan <panjiangyi@gmail.com>
2025-11-01 12:21:23 +0800

    init

---
+++ init.lua
@@ -0,0 +1,11 @@
+require('config.lazy')
+
+-- Basic settings from init.vim
+vim.opt.number = true          -- 显示绝对行号
+vim.opt.relativenumber = true  -- 显示相对行号（可选）
+vim.opt.clipboard = "unnamedplus"
+
+-- FZF mapping (you'll need to install fzf.vim plugin for this to work)
+vim.keymap.set('n', '<C-p>', ':FZF<CR')
+
+
---
+++ lua/plugins.lua
@@ -0,0 +1,9 @@
+return {
+  -- You can add your plugins here
+  -- Example:
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   tag = "0.1.5",
+  --   dependencies = { "nvim-lua/plenary.nvim" }
+  -- },
+}
