f9c4e43b4f7b3eccb3c86470df4157a2d1198f30
James Pan <panjiangyi@gmail.com>
2025-11-13 22:38:06 +0800

    自定义查询文件导致 原生只能对象失效

--- queries/cpp/textobjects.scm
+++
@@ -1,9 +0,0 @@
-;; Exception handling queries for C++
-;; Matches try-catch blocks
-
-;; @exception.outer - entire try-catch block
-(try_statement) @exception.outer
-
-;; @exception.inner - only the try block's content
-(try_statement
-  body: (compound_statement) @exception.inner)
--- queries/java/textobjects.scm
+++
@@ -1,9 +0,0 @@
-;; Exception handling queries for Java
-;; Matches try-catch-finally blocks
-
-;; @exception.outer - entire try-catch-finally block
-(try_statement) @exception.outer
-
-;; @exception.inner - only the try block's content
-(try_statement
-  body: (block) @exception.inner)
